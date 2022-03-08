c78bca8de664ad24492b9c07b5979533126ad158
danielbui78 <danielbui78@gmail.com>
2022-03-08 17:28:46 -0500

    Dzbridge namespace (#3)
    * Integrate DzBridge-Namespace changes
    * default to static linking of DzBridge-common
    * Update CMakeLists.txt
    * dzbridge.h integration

--- DazStudioPlugin/pluginmain.cpp
+++ DazStudioPlugin/pluginmain.cpp
@@ -4,30 +4,10 @@
 #include "version.h"
 #include "DzUnrealAction.h"
 #include "DzUnrealDialog.h"
-#include "DzBridgeMorphSelectionDialog.h"
-#include "DzBridgeSubdivisionDialog.h"
 
-#ifdef __APPLE__
-DZ_PLUGIN_DEFINITION("Daz To Unreal Bridge");
-#else
-BOOL WINAPI DllMain(HINSTANCE hinstDLL, ULONG fdwReason, LPVOID lpvReserved)
-{
-	switch (fdwReason) {
-	case DLL_PROCESS_ATTACH:
-		break;
-	case DLL_THREAD_ATTACH:
-		break;
-	case DLL_THREAD_DETACH:
-		break;
-	case DLL_PROCESS_DETACH:
-		break;
-	}
-	return TRUE;
-}
-static DzPlugin s_pluginDef("Daz To Unreal Bridge");
-extern "C" __declspec(dllexport) DzVersion getSDKVersion() { return DZ_SDK_VERSION; }
-extern "C" __declspec(dllexport) DzPlugin * getPluginDefinition() { return &s_pluginDef; }
-#endif
+#include "dzbridge.h"
+
+CPP_PLUGIN_DEFINITION("Daz To Unreal Bridge");
 
 DZ_PLUGIN_AUTHOR("Daz 3D, Inc");
 
@@ -40,30 +20,7 @@ Bridge Collaboration Project<br><br>\
 ).arg(PLUGIN_MAJOR).arg(PLUGIN_MINOR).arg(PLUGIN_REV).arg(PLUGIN_BUILD));
 
 DZ_PLUGIN_CLASS_GUID(DzUnrealAction, 99F42CAE-CD02-49BC-A7CE-C0CF4EDD7609);
-DZ_PLUGIN_CUSTOM_CLASS_GUID(DzUnrealDialog, b7c0b573-bd61-452c-92c1-9560459b4e89);
-
-static QWidget* GetParentArg0(const QVariantList& args)
-{
-	QWidget* parent = nullptr;
-	QVariant qvar = args[0];
-	QObject* obj = qvar.value<QObject*>();
-	if (obj && obj->inherits("QWidget"))
-	{
-		parent = (QWidget*)obj;
-	}
-
-	return parent;
-}
-
-QObject* DzUnrealDialogFactory::createInstance(const QVariantList& args) const
-{
-	QWidget* parent = GetParentArg0(args);
-	return (QObject*) new DzUnrealDialog(parent);
-}
-QObject* DzUnrealDialogFactory::createInstance() const
-{
-	return (QObject*) new DzUnrealDialog(nullptr);
-}
+NEW_PLUGIN_CUSTOM_CLASS_GUID(DzUnrealDialog, b7c0b573-bd61-452c-92c1-9560459b4e89);
 
 #ifdef UNITTEST_DZBRIDGE
 
